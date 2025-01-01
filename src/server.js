a3070f0d6b920e332f843edac34149c9d8879439
Dileep <190050003ecm@gmail.com>
2025-01-01 09:32:32 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors= require('cors');
-const router = require('./routes/userRoutes')
+const router = require('../routes/userRoutes')
 
 const app =express();
 const port = process.env.PORT || 5200;
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json())
 
 
-app.use('/users', router);
+app.use('/.netlify/functions/api', router);
 
 app.listen(port, ()=>{
     console.log(`Server running on http://localhost:${port}`);
